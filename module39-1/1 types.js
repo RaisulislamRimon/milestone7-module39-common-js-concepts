ea2aea520e1a03ef388464663f8143e9d5616d1e
RaisulislamRimon <rimonppip0@gmail.com>
2022-09-07 23:23:09 +0600

    update

---
+++ module39-1/1 types.js
@@ -0,0 +1,9 @@
+const arr = { name: "Rimon", job: "student & WEB Developer", age: 22 };
+console.log(arr);
+console.log(typeof arr);
+
+// const arr1 = [{ name: "Rimon", job: "student & WEB Developer", age: 22 }];
+const arr1 = [10, 20, 30, 40, 50];
+console.log(arr1);
+console.log(typeof arr1);
+console.log(isNaN(arr1));
