fc8dba2e4e029ec67e8734552bc6d381a6d5b431
RaisulislamRimon <rimonppip0@gmail.com>
2022-09-08 06:47:43 +0600

    update

--- module39-2/1 types.js
+++ module39-2/1 types.js
@@ -10,12 +10,13 @@
 
 // primitive type
 const a = 5;
-const b = "Samsu kopai na ekhon ar";
+const b = 'Samsu kopai na ekhon ar';
 const d = true;
 
-// non-primitive
+
+// non-primitive 
 const ages = [45, 65, 48];
-const student = { id: 23, class: 7 };
+const student = {id: 23, class: 7};
 
 // console.log(typeof a, typeof b, typeof d, typeof ages, typeof student);
 
@@ -25,7 +26,7 @@ console.log(x, y);
 y = 7;
 console.log(x, y);
 
-let p = { job: "web developer" };
+let p = {job: 'web developer'}
 let q = p;
-q.job = "front end developer";
+q.job = 'front end developer';
 console.log(p, q);
