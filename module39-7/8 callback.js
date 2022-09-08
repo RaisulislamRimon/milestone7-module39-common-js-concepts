a9b58be56f0ceece2338958e8fe613b01621bfba
RaisulislamRimon <rimonppip0@gmail.com>
2022-09-08 16:38:33 +0600

    update

--- module39-7/8 callback.js
+++ module39-7/8 callback.js
@@ -1,29 +1,60 @@
-function greeting(greetingHandler, name) {
-  greetingHandler(name);
-}
-// const name = 'Halim mama';
-// const numbers = [45, 54, 78];
-// const laptop = {price: 45000, brand: 'lenovo', memory: '8gb'};
-function greetingHandler(name) {
-  console.log("Good Morning", name);
+// function greeting(greetingHandler, name) {
+//   greetingHandler(name);
+// }
+// // const name = 'Halim mama';
+// // const numbers = [45, 54, 78];
+// // const laptop = {price: 45000, brand: 'lenovo', memory: '8gb'};
+// function greetingHandler(name) {
+//   console.log("Good Morning", name);
+// }
+
+// function greetEvening(name) {
+//   console.log("Good Evening", name);
+// }
+
+// function greetNight(name) {
+//   console.log("Good Night", name);
+// }
+
+// greeting(greetingHandler, "Tom Hanks");
+// greeting(greetingHandler, "Tom Brady");
+// greeting(greetingHandler, "Tom Cruise");
+// greeting(greetEvening, "Tom Solaiman");
+// greeting(greetEvening, "Tom Salman");
+// greeting(greetNight, "Rasel");
+
+// function submitHandler() {
+//   console.log("submit button clicked");
+// }
+// document.getElementById("btn-submit").addEventListener("click", submitHandler);
+
+function greeting(greetingHandler) {
+  console.log(greetingHandler());
 }
 
-function greetEvening(name) {
-  console.log("Good Evening", name);
+function greetingHandler() {
+  console.log("Good Morning");
 }
 
-function greetNight(name) {
-  console.log("Good Night", name);
+function greetEvening() {
+  console.log("Good Evening");
 }
 
-greeting(greetingHandler, "Tom Hanks");
-greeting(greetingHandler, "Tom Brady");
-greeting(greetingHandler, "Tom Cruise");
-greeting(greetEvening, "Tom Solaiman");
-greeting(greetEvening, "Tom Salman");
-greeting(greetNight, "Rasel");
+greeting(greetEvening);
 
-function submitHandler() {
-  console.log("submit button clicked");
-}
-document.getElementById("btn-submit").addEventListener("click", submitHandler);
+greeting(greetingHandler);
+
+// greeting("Rimon");
+
+// const personName = "Rimon";
+// greeting(personName);
+
+// const number = 45;
+// greeting(number);
+
+// const nums = [45, 54, 78];
+// greeting(nums);
+
+// const names = { brand: "Lenovo", price: 45000, memory: "8gb" };
+// greeting(names.brand);
+// greeting(names);
---
+++ module39-7/tempCodeRunnerFile.js
@@ -0,0 +1,15 @@
+
+// greeting("Rimon");
+
+// const personName = "Rimon";
+// greeting(personName);
+
+// const number = 45;
+// greeting(number);
+
+// const nums = [45, 54, 78];
+// greeting(nums);
+
+// const names = { brand: "Lenovo", price: 45000, memory: "8gb" };
+// greeting(names.brand);
+// greeting(names);
